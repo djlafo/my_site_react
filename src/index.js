6972590d1bd907f9e8a827e72cd6e0577550217d
Dylan Lafont <me@dylanlafont.com>
2019-02-23 09:58:39 -0600

    add appspec as artifact

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css'
 import * as serviceWorker from './serviceWorker';
 
 const wip = (
-<h1> 
-    This page is a work in progress
-</h1>
+    <div>
+        <h1> 
+            This page is a work in progress
+        </h1>
+        <h3>
+            hosted on an AWS EC2 instance<br/>
+            <a href="https://github.com/djlafo/my_site_react">source</a>
+        </h3>
+        <h4>
+            My links:<br/>
+            <a href="https://github.com/djlafo">GitHub</a><br/>
+            <a href="https://www.linkedin.com/in/dylan-lafont-99a58a150/">Linkedin</a>
+        </h4>
+    </div>
 );
 
 ReactDOM.render(wip,document.getElementById('root'));
