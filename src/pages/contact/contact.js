a25ef486201cb704b138815d7d3bc00c139585f7
Dylan Lafont <me@dylanlafont.com>
2019-02-23 19:20:11 -0600

    add cards, welcome message to main page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 import './contact.css';
 
+import Card from '../../components/card/card.js';
+
 class Contact extends Component {
     render() {
         return (
             <div className="contact">
-                <h4>
-                    My links:<br/>
-                    <a target="_blank" 
-                        rel="noopener noreferrer"
-                        href="https://github.com/djlafo">
-                        GitHub
-                    </a>
-                    <br/>
-                    <a target="_blank" 
-                        rel="noopener noreferrer"
-                        href="https://www.linkedin.com/in/dylan-lafont-99a58a150/">
-                        Linkedin
-                    </a>
-                    <br/>
-                    <br/>
-                    Contact me
-                    <br/>
-                    Email: <a href="mailto:me@dylanlafont.com">me@dylanlafont.com</a>
-                </h4>
+                <Card>
+                    <div>
+                        My Links:<br/>
+                        <a target="_blank" 
+                            rel="noopener noreferrer"
+                            href="https://github.com/djlafo">
+                            GitHub
+                        </a>
+                        <br/>
+                        <a target="_blank" 
+                            rel="noopener noreferrer"
+                            href="https://www.linkedin.com/in/dylan-lafont-99a58a150/">
+                            Linkedin
+                        </a>
+                        <br/>
+                        <br/>
+                        Contact me
+                        <br/>
+                        Email: <a href="mailto:me@dylanlafont.com">me@dylanlafont.com</a>
+                    </div>
+                </Card>
             </div>
         );
     }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './home.css'
 
+import Card from '../../components/card/card.js';
+import yup from '../../images/yup.png'
+
 class Home extends Component {
     render() {
         return (
             <div className="home">
-                <h1>Welcome</h1>
+                <Card>
+                    <div class="welcome-card">
+                        Welcome<br/>
+                        <br/>
+                        The website is currently undergoing improvements;
+                        Depending on my motivation at least
+                        <br/>
+                        <img class="framed" src={yup}/>
+                    </div>
+                </Card>
             </div>
         );
     }
